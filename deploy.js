1e9b8c3b37ab1575568180ab5a128d1886127eb8
rtpa25 <pandaronit25@gmail.com>
2022-03-29 08:16:02 +0530

    bumped up the solc version and made changes accordingly

--- deploy.js
+++ deploy.js
@@ -2,12 +2,16 @@
 
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
 
-const provider = new HDWallterProvider(
+const { abi, evm } = require('./compile');
+
+require('dotenv').config();
+
+const provider = new HDWalletProvider(
   process.env.MNEMONIC,
   process.env.RINKBEY_CONNECTION_URI
 );
+
 const web3 = new Web3(provider);
 
 const deploy = async () => {
@@ -15,10 +19,11 @@ const deploy = async () => {
 
   console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
     .send({ gas: '1000000', from: accounts[0] });
 
+  console.log(abi);
   console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
 };
